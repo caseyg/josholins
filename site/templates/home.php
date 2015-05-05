73bf48cf7055a86d3d0009e84f8e503398271de0
Casey Gollan <hello@caseyagollan.com>
2015-05-05 14:14:01 -0400

    simplifying

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,7 +9,11 @@
 
     <hr>
 
-    <?php snippet('projects') ?>
+      <?php foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
+      <figure>
+        <img src="<?php echo $image->url() ?>" alt="<?php echo $page->title()->html() ?>">
+      </figure>
+      <?php endforeach ?>
 
   </main>
 
