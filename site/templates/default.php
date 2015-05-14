3dcd9dc350b025fb27fe3b7a8e2d4053148cb606
Casey Gollan <hello@caseyagollan.com>
2015-05-14 12:19:30 -0400

    remove left/right nav from info/index, closes #8

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,21 +3,21 @@
 <div id="fullpage">
     <div class="section" id="cover" onClick="$.fn.fullpage.moveSectionDown();"><h1>Josh Olins</h1></div>
     <div class="section"><?php snippet('gallery') ?></div>
-    <div class="section">
+    <div class="section no-nav">
       <div class="slide" data-anchor="information">
-		<article>
-			<h2>Information</h2>
-			<p><?php echo $site->information() ?></p>
-		</article>
-		<footer style="display:none;">
-			<p><?php echo $site->address() ?></p>
-			<p><a href="mailto:<?php echo $site->email() ?>"><?php echo $site->email() ?></a></p>
-     	</footer>
+    		<article>
+    			<h2>Information</h2>
+    			<p><?php echo $site->information() ?></p>
+    		</article>
+    		<footer style="display:none;">
+    			<p><?php echo $site->address() ?></p>
+    			<p><a href="mailto:<?php echo $site->email() ?>"><?php echo $site->email() ?></a></p>
+       	</footer>
       </div>
       <div class="slide" data-anchor="index">
         <?php snippet('index') ?>
       </div>      
-    </div>
+  </div>
 </div>
 
 <?php snippet('footer') ?>
